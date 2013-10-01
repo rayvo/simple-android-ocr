c888a1d8f9bf4f9f150b3cc67afc9b4f12066fd3
voquocduy@gmail.com <voquocduy@gmail.com@5a6e7e14-450c-98a8-01f5-7bab550a6b2e>
2013-10-01 05:19:44 +0000

    Update Select Photo Button

---  simple-android-ocr --username voquocduy@gmail.com/Simple-Android-OCR/src/com/datumdroid/android/ocr/simple/SimpleAndroidOCRActivity.java
+++  simple-android-ocr --username voquocduy@gmail.com/Simple-Android-OCR/src/com/datumdroid/android/ocr/simple/SimpleAndroidOCRActivity.java
@@ -2,6 +2,7 @@ package com.datumdroid.android.ocr.simple;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,6 +15,7 @@ import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.graphics.Rect;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
@@ -30,7 +32,7 @@ public class SimpleAndroidOCRActivity extends Activity {
 	public static final String PACKAGE_NAME = "com.datumdroid.android.ocr.simple";
 	public static final String DATA_PATH = Environment
 			.getExternalStorageDirectory().toString() + "/SimpleAndroidOCR/";
-	
+
 	// You should have the trained data file in assets folder
 	// You can get them at:
 	// http://code.google.com/p/tesseract-ocr/downloads/list
@@ -39,12 +41,14 @@ public class SimpleAndroidOCRActivity extends Activity {
 	private static final String TAG = "SimpleAndroidOCR.java";
 
 	protected Button _button;
+	protected Button _btnSelectPhoto;
 	// protected ImageView _image;
 	protected EditText _field;
 	protected String _path;
 	protected boolean _taken;
 
 	protected static final String PHOTO_TAKEN = "photo_taken";
+	private static final int SELECT_PHOTO = 100;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -55,7 +59,8 @@ public class SimpleAndroidOCRActivity extends Activity {
 			File dir = new File(path);
 			if (!dir.exists()) {
 				if (!dir.mkdirs()) {
-					Log.v(TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
+					Log.v(TAG, "ERROR: Creation of directory " + path
+							+ " on sdcard failed");
 					return;
 				} else {
 					Log.v(TAG, "Created directory " + path + " on sdcard");
@@ -63,34 +68,38 @@ public class SimpleAndroidOCRActivity extends Activity {
 			}
 
 		}
-		
+
 		// lang.traineddata file with the app (in assets folder)
 		// You can get them at:
 		// http://code.google.com/p/tesseract-ocr/downloads/list
 		// This area needs work and optimization
-		if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata")).exists()) {
+		if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata"))
+				.exists()) {
 			try {
 
 				AssetManager assetManager = getAssets();
-				InputStream in = assetManager.open("tessdata/" + lang + ".traineddata");
-				//GZIPInputStream gin = new GZIPInputStream(in);
-				OutputStream out = new FileOutputStream(DATA_PATH
-						+ "tessdata/" + lang + ".traineddata");
+				InputStream in = assetManager.open("tessdata/" + lang
+						+ ".traineddata");
+				// GZIPInputStream gin = new GZIPInputStream(in);
+				OutputStream out = new FileOutputStream(DATA_PATH + "tessdata/"
+						+ lang + ".traineddata");
 
 				// Transfer bytes from in to out
 				byte[] buf = new byte[1024];
 				int len;
-				//while ((lenf = gin.read(buff)) > 0) {
+				// while ((lenf = gin.read(buff)) > 0) {
 				while ((len = in.read(buf)) > 0) {
 					out.write(buf, 0, len);
 				}
 				in.close();
-				//gin.close();
+				// gin.close();
 				out.close();
-				
+
 				Log.v(TAG, "Copied " + lang + " traineddata");
 			} catch (IOException e) {
-				Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
+				Log.e(TAG,
+						"Was unable to copy " + lang + " traineddata "
+								+ e.toString());
 			}
 		}
 
@@ -103,6 +112,9 @@ public class SimpleAndroidOCRActivity extends Activity {
 		_button = (Button) findViewById(R.id.button);
 		_button.setOnClickListener(new ButtonClickHandler());
 
+		_btnSelectPhoto = (Button) findViewById(R.id.btnSelectPhoto);
+		_btnSelectPhoto.setOnClickListener(new SelectButtonClickHandler());
+
 		_path = DATA_PATH + "/ocr.jpg";
 	}
 
@@ -110,6 +122,15 @@ public class SimpleAndroidOCRActivity extends Activity {
 		public void onClick(View view) {
 			Log.v(TAG, "Starting Camera app");
 			startCameraActivity();
+		}
+	}
+
+	public class SelectButtonClickHandler implements View.OnClickListener {
+		public void onClick(View view) {
+			Log.v(TAG, "Start selecting a Photo");
+			Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+			photoPickerIntent.setType("image/*");
+			startActivityForResult(photoPickerIntent, SELECT_PHOTO);
 		}
 	}
 
@@ -128,14 +149,42 @@ public class SimpleAndroidOCRActivity extends Activity {
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
+		super.onActivityResult(requestCode, resultCode, data);
 		Log.i(TAG, "resultCode: " + resultCode);
 
-		if (resultCode == -1) {
-			onPhotoTaken();
-		} else {
-			Log.v(TAG, "User cancelled");
+		switch (requestCode) {
+		case SELECT_PHOTO:
+			Bitmap yourSelectedImage = null;
+			if (resultCode == RESULT_OK) {
+				Uri selectedImage = data.getData();
+				InputStream imageStream;
+				try {
+					imageStream = getContentResolver().openInputStream(
+							selectedImage);
+					
+					BitmapFactory.Options options = new BitmapFactory.Options();
+					options.inSampleSize = 4;					
+					
+					yourSelectedImage = BitmapFactory.decodeStream(imageStream,null, options );
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+			if (yourSelectedImage != null) {
+				onPhotoTaken(yourSelectedImage);
+			}
+			break;
+		case 0:
+			if (resultCode == -1) {
+				onPhotoTaken(null);
+			} else {
+				Log.v(TAG, "User cancelled");
+			}
+			break;
 		}
+
 	}
 
 	@Override
@@ -147,96 +196,114 @@ public class SimpleAndroidOCRActivity extends Activity {
 	protected void onRestoreInstanceState(Bundle savedInstanceState) {
 		Log.i(TAG, "onRestoreInstanceState()");
 		if (savedInstanceState.getBoolean(SimpleAndroidOCRActivity.PHOTO_TAKEN)) {
-			onPhotoTaken();
+			onPhotoTaken(null);
 		}
 	}
 
-	protected void onPhotoTaken() {
+	protected void onPhotoTaken(Bitmap bitmap) {
 		_taken = true;
-
 		BitmapFactory.Options options = new BitmapFactory.Options();
 		options.inSampleSize = 4;
+		
+		if (bitmap == null) {			
 
-		Bitmap bitmap = BitmapFactory.decodeFile(_path, options);
+			bitmap = BitmapFactory.decodeFile(_path, options);
 
-		try {
-			ExifInterface exif = new ExifInterface(_path);
-			int exifOrientation = exif.getAttributeInt(
-					ExifInterface.TAG_ORIENTATION,
-					ExifInterface.ORIENTATION_NORMAL);
+			try {
+				ExifInterface exif = new ExifInterface(_path);
+				int exifOrientation = exif.getAttributeInt(
+						ExifInterface.TAG_ORIENTATION,
+						ExifInterface.ORIENTATION_NORMAL);
 
-			Log.v(TAG, "Orient: " + exifOrientation);
+				Log.v(TAG, "Orient: " + exifOrientation);
 
-			int rotate = 0;
+				int rotate = 0;
 
-			switch (exifOrientation) {
-			case ExifInterface.ORIENTATION_ROTATE_90:
-				rotate = 90;
-				break;
-			case ExifInterface.ORIENTATION_ROTATE_180:
-				rotate = 180;
-				break;
-			case ExifInterface.ORIENTATION_ROTATE_270:
-				rotate = 270;
-				break;
+				switch (exifOrientation) {
+				case ExifInterface.ORIENTATION_ROTATE_90:
+					rotate = 90;
+					break;
+				case ExifInterface.ORIENTATION_ROTATE_180:
+					rotate = 180;
+					break;
+				case ExifInterface.ORIENTATION_ROTATE_270:
+					rotate = 270;
+					break;
+				}
+
+				Log.v(TAG, "Rotation: " + rotate);
+
+				if (rotate != 0) {
+
+					// Getting width & height of the given image.
+					int w = bitmap.getWidth();
+					int h = bitmap.getHeight();
+
+					// Setting pre rotate
+					Matrix mtx = new Matrix();
+					mtx.preRotate(rotate);
+
+					// Rotating Bitmap
+					bitmap = Bitmap
+							.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+				}
+
+				// Convert to ARGB_8888, required by tess
+				bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+			} catch (IOException e) {
+				Log.e(TAG, "Couldn't correct orientation: " + e.toString());
 			}
-
-			Log.v(TAG, "Rotation: " + rotate);
-
-			if (rotate != 0) {
-
-				// Getting width & height of the given image.
-				int w = bitmap.getWidth();
-				int h = bitmap.getHeight();
-
-				// Setting pre rotate
-				Matrix mtx = new Matrix();
-				mtx.preRotate(rotate);
-
-				// Rotating Bitmap
-				bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
-			}
-
-			// Convert to ARGB_8888, required by tess
+		} else {			
 			bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
-
-		} catch (IOException e) {
-			Log.e(TAG, "Couldn't correct orientation: " + e.toString());
 		}
 
 		// _image.setImageBitmap( bitmap );
-		
+
 		Log.v(TAG, "Before baseApi");
 
 		TessBaseAPI baseApi = new TessBaseAPI();
+		
 		baseApi.setDebug(true);
+		
 		baseApi.init(DATA_PATH, lang);
+		//baseApi.init(DATA_PATH, lang,TessBaseAPI.OEM_TESSERACT_ONLY);
 		baseApi.setImage(bitmap);
-		
-		String recognizedText = baseApi.getUTF8Text();
-		
+
+		String recognizedText = "";
+		try {
+			recognizedText = baseApi.getUTF8Text();
+		}catch (Exception e) {
+			Log.e(TAG, "ERROR OCRED TEXT: " + e.toString());
+			e.printStackTrace();
+		}
+
 		baseApi.end();
 
-		// You now have the text in recognizedText var, you can do anything with it.
-		// We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
+		// You now have the text in recognizedText var, you can do anything with
+		// it.
+		// We will display a stripped out trimmed alpha-numeric version of it
+		// (if lang is eng)
 		// so that garbage doesn't make it to the display.
 
 		Log.v(TAG, "OCRED TEXT: " + recognizedText);
 
-		if ( lang.equalsIgnoreCase("eng") ) {
+		if (lang.equalsIgnoreCase("eng")) {
 			recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
 		}
-		
+
 		recognizedText = recognizedText.trim();
 
-		if ( recognizedText.length() != 0 ) {
-			_field.setText(_field.getText().toString().length() == 0 ? recognizedText : _field.getText() + " " + recognizedText);
-			_field.setSelection(_field.getText().toString().length());
+		if (recognizedText.length() != 0) {
+			_field.setText(recognizedText);
+			/*_field.setText(_field.getText().toString().length() == 0 ? recognizedText
+					: _field.getText() + " " + recognizedText);
+			_field.setSelection(_field.getText().toString().length());*/
 		}
-		
+
 		// Cycle done.
 	}
-	
+
 	// www.Gaut.am was here
 	// Thanks for reading!
 }
